90dd0cf51ac9c5f38b2807340ea718248d3a6299
Jared Randall <jcr8969@rit.edu>
2024-04-28 12:19:45 -0400

    Installed the EntityFrameworkCore package and implemented the Data, so that it works with the database

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Cork_Technical.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Cork_Technical.Models;
+
+namespace Cork_Technical.Data
 {
-    public class ApiDbContext
+    public class ApiDbContext : DbContext
     {
+        public DbSet<Asset> Assets { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
     }
 }
