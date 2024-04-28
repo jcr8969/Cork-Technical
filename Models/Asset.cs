ef742849f1b03f3f16ea92dbb65e96ad94236a03
Jared Randall <jcr8969@rit.edu>
2024-04-28 12:16:24 -0400

    Implemented the cyber asset Model, which matches the database

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -2,5 +2,13 @@
 {
     public class Asset
     {
+        // components of the cyber asset
+        public int id { get; set; }
+        public string? device_name { get; set; }
+        public string? type { get; set; }
+
+        public int? serial {  get; set; }
+
+        public string? os { get; set; }
     }
 }
